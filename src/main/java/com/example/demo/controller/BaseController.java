e084a832d4b8a78fb7510f54bb82fe51c149eb76
baeseokin <baeseokin@gmail.com>
2024-10-03 19:36:25 +0900

    logstash 파싱로직 변경

--- src/main/java/com/example/demo/controller/BaseController.java
+++ src/main/java/com/example/demo/controller/BaseController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.CustomException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,14 +16,43 @@ public abstract class BaseController {
 
     private final Logger logger = LogManager.getLogger(BaseController.class);
 
+    @Autowired
+    private HttpServletRequest request;
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handleCustomException(CustomException ex) {
+
+        String userId ="";
+        HttpSession session = request.getSession(false);
+        if(session!=null){
+            Object useIdo = session.getAttribute("userId");
+            if(useIdo!=null){
+                userId = (String)useIdo;
+            }
+        }else{
+            userId = (String)request.getParameter("userId");
+        }
+
+        String requestURI = request.getRequestURI();
+        String pageId = "";
+        if(request.getParameter("pageId")!=null){
+            pageId = (String)request.getParameter("pageId");
+        }
+        String serviceId = "";
+        if(request.getParameter("serviceId")!=null){
+            serviceId = (String)request.getParameter("serviceId");
+        }
+
         // 에러 코드, 메시지, 스택 트레이스를 로그로 출력
-        logger.error("--> Error Start");
+        logger.error("-->");
         logger.error("Error Code: {}", ex.getErrorCode());
         logger.error("Error Message: {}", ex.getMessage());
+        logger.error("UserId: {}", userId);
+        logger.error("PageId: {}", pageId);
+        logger.error("ServiceId: {}", serviceId);
+        logger.error("RequestURI: {}", requestURI);
         logger.error("Stack Trace: ", ex);
-        logger.error("--> Error End");
+        logger.error("<--");
 
         // 클라이언트에 보낼 응답
         return new ResponseEntity<>(ex.getErrorCode() + ": " + ex.getMessage(), HttpStatus.BAD_REQUEST);
