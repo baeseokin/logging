09d71a5e6f1b391729b89d3095f048c897904c53
baeseokin <baeseokin@gmail.com>
2024-10-12 09:17:24 +0900

    시스템 정보 추가

--- src/main/resources/mariadb/logging.sql
+++ src/main/resources/mariadb/logging.sql
@@ -20,46 +20,61 @@ CREATE DATABASE IF NOT EXISTS `logging` /*!40100 DEFAULT CHARACTER SET armscii8 
 USE `logging`;
 
 -- 테이블 logging.page_info 구조 내보내기
+DROP TABLE IF EXISTS `page_info`;
 CREATE TABLE IF NOT EXISTS `page_info` (
   `page_id` varchar(50) NOT NULL,
   `page_nm` varchar(100) DEFAULT NULL,
   `use_yn` char(1) NOT NULL,
   `block_yn` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- 테이블 데이터 logging.page_info:~2 rows (대략적) 내보내기
 INSERT INTO `page_info` (`page_id`, `page_nm`, `use_yn`, `block_yn`) VALUES
-	('P00001', '테스트화면1', 'Y', 'N'),
-	('P00002', '테스트화면2', 'Y', 'N');
+	('PGEA0100001', '가입설계처리', 'Y', 'N'),
+	('PGEB0100001', '계약조회', 'Y', 'N');
 
 -- 테이블 logging.page_service_mapping 구조 내보내기
+DROP TABLE IF EXISTS `page_service_mapping`;
 CREATE TABLE IF NOT EXISTS `page_service_mapping` (
   `page_id` varchar(50) NOT NULL,
   `service_id` varchar(50) NOT NULL,
   `use_yn` char(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- 테이블 데이터 logging.page_service_mapping:~4 rows (대략적) 내보내기
 INSERT INTO `page_service_mapping` (`page_id`, `service_id`, `use_yn`) VALUES
-	('P00001', 'S00001', 'Y'),
-	('P00001', 'S00002', 'Y'),
-	('P00002', 'S00003', 'Y'),
-	('P00002', 'S00004', 'Y');
+	('PGEA0100001', 'RSTA0100001', 'Y'),
+	('PGEA0100001', 'RSTA0100002', 'Y'),
+	('PGEB0100001', 'RSTB0100001', 'Y'),
+	('PGEB0100001', 'RSTB0100002', 'Y');
 
 -- 테이블 logging.service_info 구조 내보내기
+DROP TABLE IF EXISTS `service_info`;
 CREATE TABLE IF NOT EXISTS `service_info` (
   `service_id` varchar(50) NOT NULL,
   `service_nm` varchar(100) DEFAULT NULL,
   `use_yn` char(1) NOT NULL,
   `block_yn` char(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- 테이블 데이터 logging.service_info:~4 rows (대략적) 내보내기
 INSERT INTO `service_info` (`service_id`, `service_nm`, `use_yn`, `block_yn`) VALUES
-	('S00001', '테스트서비스1', 'Y', 'N'),
-	('S00002', '테스트서비스2', 'Y', 'N'),
-	('S00003', '테스트서비스3', 'Y', 'N'),
-	('S00004', '테스트서비스4', 'Y', 'N');
+	('RSTA0100001', '기존가입설계내역조회', 'Y', 'N'),
+	('RSTA0100002', '가입설계 생성', 'Y', 'N'),
+	('RSTB0100001', '고객정보조회', 'Y', 'N'),
+	('RSTB0100002', '계약정보조회', 'Y', 'N');
+
+-- 테이블 logging.system_info 구조 내보내기
+DROP TABLE IF EXISTS `system_info`;
+CREATE TABLE IF NOT EXISTS `system_info` (
+  `system_id` varchar(50) NOT NULL DEFAULT '',
+  `system_nm` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- 테이블 데이터 logging.system_info:~2 rows (대략적) 내보내기
+INSERT INTO `system_info` (`system_id`, `system_nm`) VALUES
+	('A01', '가입설계'),
+	('B01', '계약관리');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
